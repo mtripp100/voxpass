75d87c01559f65ed99b20bee324d60cb23f326be
Magnus Tripp <mtripp100@gmail.com>
2018-01-12 21:27:09 +0000

    Tidy up.

--- voxpass/voxpass.py
+++ voxpass/voxpass.py
@@ -1,30 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import string
 import random
 import click
 
+VOWELS = ('a', 'e', 'i', 'o', 'u')
+CONSONANTS = tuple(x for x in string.ascii_lowercase if x not in VOWELS)
 
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = [x for x in list(string.ascii_lowercase) if x not in vowels]
-crypto = random.SystemRandom()
 
 @click.command()
-@click.option('--num_vowels', type=click.INT, default=3, help='Number of vowels in password (default = 3).')
-@click.option('--allow-dups', is_flag=True, help='Allow duplicate letters in password.')
-def genpass(num_vowels, allow_dups):
+@click.option('--num-vowels', type=click.INT, default=3, help='Number of vowels in password.', show_default=True)
+def genpass(num_vowels):
     """Generates passwords in CONSONANT-VOWEL-CONSONANT format for high readability."""
-    letters = gen_with_dups(num_vowels) if allow_dups else gen_without_dups(num_vowels)
+    rand = random.SystemRandom()
+    letters = (rand.choice(VOWELS if x % 3 == 1 else CONSONANTS) for x in range(num_vowels * 3))
     click.echo(''.join(letters))
 
-def gen_with_dups(num_vowels):
-    return [crypto.choice(vowels if x % 3 == 1 else consonants) for x in range(num_vowels * 3)]
-
-def gen_without_dups(num_vowels):
-    sampled_vowels = crypto.sample(vowels, num_vowels)
-    sampled_consonants = crypto.sample(consonants, num_vowels * 2)
-
-    return [sampled_vowels.pop() if x % 3 == 1 else sampled_consonants.pop() for x in range(num_vowels * 3)]
 
 if __name__ == '__main__':
-    genpass()
+    genpass()  # pylint: disable=E1120
