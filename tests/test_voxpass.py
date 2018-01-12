1691b44d9c4fd3fed80a1476f58334b70e6bd128
Magnus Tripp <mtripp100@gmail.com>
2018-01-12 21:40:24 +0000

    Tests.

---
+++ tests/test_voxpass.py
@@ -0,0 +1,9 @@
+from voxpass import voxpass
+import pytest
+
+
+@pytest.mark.parametrize("num_vowels, pwd_length", [
+    (0, 0), (1, 3), (3, 9), (50, 150)
+])
+def test_length(num_vowels, pwd_length):
+    assert len(voxpass.generate_password(num_vowels)) == pwd_length
--- voxpass/voxpass.py
+++ voxpass/voxpass.py
@@ -10,12 +10,16 @@ CONSONANTS = tuple(x for x in string.ascii_lowercase if x not in VOWELS)
 
 @click.command()
 @click.option('--num-vowels', type=click.INT, default=3, help='Number of vowels in password.', show_default=True)
-def genpass(num_vowels):
+def cli(num_vowels):
     """Generates passwords in CONSONANT-VOWEL-CONSONANT format for high readability."""
+    click.echo(generate_password(num_vowels))
+
+
+def generate_password(num_vowels):
     rand = random.SystemRandom()
     letters = (rand.choice(VOWELS if x % 3 == 1 else CONSONANTS) for x in range(num_vowels * 3))
-    click.echo(''.join(letters))
+    return ''.join(letters)
 
 
 if __name__ == '__main__':
-    genpass()  # pylint: disable=E1120
+    cli()  # pylint: disable=E1120
