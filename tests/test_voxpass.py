ffa6fdaf7833f354cb35991e5a30e5525821e3e3
Magnus Tripp <mtripp100@gmail.com>
2020-04-26 12:42:26 +0100

    Reformatted with black.

--- tests/test_voxpass.py
+++ tests/test_voxpass.py
@@ -3,8 +3,6 @@ import pytest
 from voxpass import voxpass
 
 
-@pytest.mark.parametrize('num_vowels, pwd_length', [
-    (0, 0), (1, 3), (3, 9), (50, 150)
-])
+@pytest.mark.parametrize("num_vowels, pwd_length", [(0, 0), (1, 3), (3, 9), (50, 150)])
 def test_length(num_vowels, pwd_length):
     assert len(voxpass.generate_password(num_vowels)) == pwd_length
--- voxpass/__main__.py
+++ voxpass/__main__.py
@@ -4,11 +4,17 @@ from .voxpass import generate_password
 
 
 @click.command()
-@click.option('--num-vowels', type=click.INT, default=3, help='Number of vowels in password.', show_default=True)
+@click.option(
+    "--num-vowels",
+    type=click.INT,
+    default=3,
+    help="Number of vowels in password.",
+    show_default=True,
+)
 def run(num_vowels):
     """Generates passwords in CONSONANT-VOWEL-CONSONANT format for high readability."""
     click.echo(generate_password(num_vowels))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()  # pylint: disable=no-value-for-parameter
