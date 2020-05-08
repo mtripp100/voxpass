ea0263e119f7c548f99f67f8df9361a4e5a9ffbb
Magnus Tripp <mtripp100@gmail.com>
2020-05-08 15:58:24 +0100

    Move to src layout.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="voxpass",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     install_requires=["click"],
     entry_points={"console_scripts": {"voxpass = voxpass.__main__:run"}},
 )
