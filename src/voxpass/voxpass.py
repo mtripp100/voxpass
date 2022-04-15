4176aaca862b4d2c0e6db4f58523b6e630c19497
Magnus Tripp <mtripp100@gmail.com>
2022-04-15 17:50:15 +0100

    Formatting updates.

--- src/voxpass/voxpass.py
+++ src/voxpass/voxpass.py
@@ -7,7 +7,5 @@ CONSONANTS = tuple(x for x in string.ascii_lowercase if x not in VOWELS)
 
 def generate_password(num_vowels):
     rand = random.SystemRandom()
-    letters = (
-        rand.choice(VOWELS if x % 3 == 1 else CONSONANTS) for x in range(num_vowels * 3)
-    )
+    letters = (rand.choice(VOWELS if x % 3 == 1 else CONSONANTS) for x in range(num_vowels * 3))
     return "".join(letters)
